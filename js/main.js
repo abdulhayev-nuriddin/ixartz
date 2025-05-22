6fe0a4ffd4a8cc7d1f47a55e328897878ff5d015
Nuriddin <abdulxayevnuriddin01@gmail.com>
2025-05-22 23:51:38 +0500

    projects and posts end

--- js/main.js
+++ js/main.js
@@ -34,3 +34,47 @@ document.addEventListener("DOMContentLoaded", () => {
   hideLoader();
   updateScrollProgressBar();
 });
+
+// -----------------------------------------------------------------------------
+
+const track = document.querySelector(".carousel-track");
+const cards = document.querySelectorAll(".card");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+let currentSlide = 0;
+
+function updateSlides() {
+  let cardsPerSlide;
+  if (window.innerWidth <= 480) {
+    cardsPerSlide = 1;
+  } else if (window.innerWidth <= 768) {
+    cardsPerSlide = 2;
+  } else {
+    cardsPerSlide = 3;
+  }
+
+  const totalSlides = Math.ceil(cards.length / cardsPerSlide);
+
+  if (currentSlide >= totalSlides) {
+    currentSlide = 0;
+  } else if (currentSlide < 0) {
+    currentSlide = totalSlides - 1;
+  }
+
+  const slideWidth = 100 / cardsPerSlide;
+  const offset = -(currentSlide * 100);
+  track.style.transform = `translateX(${offset}%)`;
+}
+
+prevBtn.addEventListener("click", () => {
+  currentSlide--;
+  updateSlides();
+});
+
+nextBtn.addEventListener("click", () => {
+  currentSlide++;
+  updateSlides();
+});
+
+window.addEventListener("resize", updateSlides);
+updateSlides();
